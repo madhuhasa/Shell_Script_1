0d65c733a2b346f8f350c02c42bbd19d54256809
Madhuhasa <madhuhasareddy@gmail.com>
2018-07-11 12:25:46 +0530

    Delete task3.sh

--- task3.sh
+++
@@ -1,9 +0,0 @@
-wget --user jenkins --password redhat http://18.219.209.240/tmanserver_BUILD_1234.zip #downloading
-unzip tmanserver_BUILD_1234.zip #unzipping
-content=$(cat tmanserver_BUILD_1234/bin/*.txt)
-echo "$content"
-if [[ tmanserver_BUILD_1234 = *"$content"* ]]; then
-  echo "Same build files"
-else
- echo "Different build files"
-fi
